cfcaeaaf0f8efd2d2d7bbcc8e1d8c70c965924cd
PizzaNCoding88 <110925628+PizzaNCoding88@users.noreply.github.com>
2023-11-13 17:46:32 +0000

    top part design v1.5

--- app/components/TopSection.jsx/TopSection.jsx
+++ app/components/TopSection.jsx/TopSection.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Top from "./TopSection.module.css";
+import Image from "next/image";
+import sunIcon from "../../../public/assets/sun-icon.png";
 
 const TopSection = () => {
   return (
     <div className={Top.main}>
-      <div className={Top.overlay}></div>
+      {/* <div className={Top.overlay}></div> */}
       <div className={Top.top}>
         <div className={Top.date}> 13th November, Monday</div>
         <div className={Top.position}>
@@ -13,7 +15,9 @@ const TopSection = () => {
         </div>
       </div>
       <div className={Top.bottom}>
-        <div></div>
+        <div>
+          <Image alt="weather icon" src={sunIcon}></Image>
+        </div>
         <div className={Top.bottomright}>
           <div className={Top.temp}>8&deg;C</div>
           <div>Sunny</div>
