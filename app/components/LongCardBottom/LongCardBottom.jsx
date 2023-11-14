b4125ea1b25ecdb4c0a8376d8411254f2f14c7b3
PizzaNCoding88 <110925628+PizzaNCoding88@users.noreply.github.com>
2023-11-14 16:47:03 +0000

    bottom completed

--- app/components/LongCardBottom/LongCardBottom.jsx
+++ app/components/LongCardBottom/LongCardBottom.jsx
@@ -1,16 +1,23 @@
 import React from "react";
 import LongCardBottom from "./LongCardBottom.module.css";
+import sunriseIcon from "../../../public/assets/sunrise-icon.png";
+import sunsetIcon from "../../../public/assets/sunset-icon.png";
+import Image from "next/image";
 
 const LongCard = () => {
   return (
     <div className={LongCardBottom.container}>
-      <div>
-        <div></div>
-        <div></div>
+      <div className={LongCardBottom.left}>
+        <div>
+          <Image alt="sunrise icon" src={sunriseIcon} />
+        </div>
+        <p>6:58AM</p>
       </div>
-      <div>
-        <div></div>
-        <div></div>
+      <div className={LongCardBottom.right}>
+        <div>
+          <Image alt="sunset icon" src={sunsetIcon} />
+        </div>
+        <p>04:29PM</p>
       </div>
     </div>
   );
